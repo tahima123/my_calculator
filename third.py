30caaa33b742f2367e69c079ac897e70a57ed331
tahima123 <tahimaakter.75@gmail.com>
2024-10-24 13:40:38 +0600

    Add files via upload

---
+++ third.py
@@ -0,0 +1,12 @@
+a=int(input("enter number one:"))
+b=int(input("enter number two:"))
+
+#summation
+print(f'the sum is',a+b)
+# subtraction
+print(f'the subtraction is',a-b)
+
+# multiplication
+print(f'the multiplication is',a*b)
+#division
+print(f'the division is',a/b)
